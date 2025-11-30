6ab52198da4e8e481d8620dd42383ace1b8f2447
Florian Raffl <florian@raffl.tirol>
2025-11-30 16:20:15 +0100

    fix api

--- custom_components/divus_dplus/api.py
+++ custom_components/divus_dplus/api.py
@@ -26,13 +26,13 @@ class DivusDplusApi:
                             devices.append(device)
                 return devices
 
-    # async def get_state(self, device_id):
-    #     async with self._session.get(self._base + f"device/{device_id}/state") as r:
-    #         return await r.json()
+    async def get_state(self, device_id):
+        async with self._session.get(self._base + f"device/{device_id}/state") as r:
+            return await r.json()
 
-    # async def set_state(self, device_id, value):
-    #     async with self._session.post(self._base + f"device/{device_id}/set", json=value) as r:
-    #         return await r.json()
+    async def set_state(self, device_id, value):
+        async with self._session.post(self._base + f"device/{device_id}/set", json=value) as r:
+            return await r.json()
         
     async def _get_surroundings(self, surrounding_id):
         formData = {
